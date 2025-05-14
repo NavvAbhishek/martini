b3cfba48fcd17b34770d126be334af73262e31e9
NavvAbhishek <abi2000navi@gmail.com>
2025-05-14 18:44:42 +0530

    add product, inventory and order to one maven archetype

---
+++ order/src/main/java/com/example/order/service/OrderService.java
@@ -0,0 +1,47 @@
+package com.example.order.service;
+
+import com.example.order.dto.OrderDTO;
+import com.example.order.model.Orders;
+import com.example.order.repo.OrderRepo;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class OrderService {
+    @Autowired
+    private OrderRepo orderRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<OrderDTO> getAllOrders(){
+        List<Orders> orderList = orderRepo.findAll();
+        return modelMapper.map(orderList, new TypeToken<List<OrderDTO>>(){}.getType());
+    }
+
+    public OrderDTO saveOrder(OrderDTO orderDTO){
+        orderRepo.save(modelMapper.map(orderDTO, Orders.class));
+        return orderDTO;
+    }
+
+    public OrderDTO updateOrder(OrderDTO orderDTO){
+        orderRepo.save(modelMapper.map(orderDTO, Orders.class));
+        return orderDTO;
+    }
+
+    public String deleteOrder(Integer orderId){
+        orderRepo.deleteById(orderId);
+        return "Order deleted...";
+    }
+
+    public OrderDTO getOrderById (Integer orderId){
+        Orders order = orderRepo.getOrderById(orderId);
+        return modelMapper.map(order, OrderDTO.class);
+    }
+}
