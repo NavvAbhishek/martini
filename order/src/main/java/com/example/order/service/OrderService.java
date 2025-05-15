0bb54deab02f6150121babb4817b47544bde2639
NavvAbhishek <abi2000navi@gmail.com>
2025-05-15 15:35:49 +0530

    Refactor order logic to include product validation

--- order/src/main/java/com/example/order/service/OrderService.java
+++ order/src/main/java/com/example/order/service/OrderService.java
@@ -7,19 +7,23 @@ import com.example.order.common.SuccessOrderResponse;
 import com.example.order.dto.OrderDTO;
 import com.example.order.model.Orders;
 import com.example.order.repo.OrderRepo;
+import com.example.product.dto.ProductDTO;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.List;
 
 @Service
 @Transactional
 public class OrderService {
-    private final WebClient webClient;
+    private final WebClient inventoryWebClient;
+    private final WebClient productWebClient;
 
     @Autowired
     private OrderRepo orderRepo;
@@ -27,8 +31,9 @@ public class OrderService {
     @Autowired
     private ModelMapper modelMapper;
 
-    public OrderService(WebClient.Builder webClientBuilder, OrderRepo orderRepo, ModelMapper modelMapper) {
-        this.webClient = webClientBuilder.baseUrl("http://localhost:8080/api/v1").build();
+    public OrderService(WebClient inventoryWebClient,WebClient productWebClient, OrderRepo orderRepo, ModelMapper modelMapper) {
+        this.inventoryWebClient = inventoryWebClient;
+        this.productWebClient = productWebClient;
         this.orderRepo = orderRepo;
         this.modelMapper = modelMapper;
     }
@@ -43,21 +48,40 @@ public class OrderService {
          Integer itemId = orderDTO.getItemId();
 
          try{
-             InventoryDTO inventoryResponse = webClient.get()
+             InventoryDTO inventoryResponse = inventoryWebClient.get()
                      .uri(uriBuilder -> uriBuilder.path("/item/{itemId}").build(itemId))
                      .retrieve()
                      .bodyToMono(InventoryDTO.class)
                      .block();
-             if(inventoryResponse != null && inventoryResponse.getQuantity() > 0){
-                 orderRepo.save(modelMapper.map(orderDTO, Orders.class));
+
+             Integer productId = inventoryResponse.getProductId();
+
+             ProductDTO productResponse = productWebClient.get()
+                     .uri(uriBuilder -> uriBuilder.path("/product/{productId}").build(productId))
+                     .retrieve()
+                     .bodyToMono(ProductDTO.class)
+                     .block();
+
+             assert inventoryResponse != null;
+             assert productResponse != null;
+             
+             if( inventoryResponse.getQuantity() > 0){
+                 if(productResponse.getForSale() == 1){
+                     orderRepo.save(modelMapper.map(orderDTO, Orders.class));
+                 }
+                 else{
+                     return new ErrorOrderResponse("item is not for sale. Please try later") ;
+                 }
                  return new SuccessOrderResponse(orderDTO);
              }
              else{
                  return new ErrorOrderResponse("item not available. Please try later") ;
              }
          }
-         catch (Exception e){
-             e.printStackTrace();
+         catch (WebClientResponseException e){
+             if(e.getStatusCode().is5xxServerError()){
+                 return new ErrorOrderResponse("item not found") ;
+             }
          }
 
         return null;
